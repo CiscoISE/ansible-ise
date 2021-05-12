0fc648a56c2299e205b5a589cde2a21237452c7d
Rafael Campos <rcampos@altus.cr>
2021-05-12 15:18:23 -0600

    Add network_device_group modules

---
+++ plugins/action/network_device_group.py
@@ -0,0 +1,187 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from ansible.plugins.action import ActionBase
+try:
+    from ansible_collections.ansible.utils.plugins.module_utils.common.argspec_validate import (
+        AnsibleArgSpecValidator,
+    )
+except ImportError:
+    ANSIBLE_UTILS_IS_INSTALLED = False
+else:
+    ANSIBLE_UTILS_IS_INSTALLED = True
+from ansible.errors import AnsibleActionFail
+from urllib.parse import quote
+from ansible_collections.cisco.ise.plugins.module_utils.ise import (
+    ISESDK,
+    ise_argument_spec,
+)
+from ansible_collections.cisco.ise.plugins.module_utils.exceptions import (
+    InconsistentParameters,
+)
+
+# Get common arguements specification
+argument_spec = ise_argument_spec()
+# Add arguments specific for this module
+argument_spec.update(dict(
+        state = dict(type="str", default="present", choices=["present", "absent"]),
+        id=dict(type="str"),
+        name=dict(type="str"),
+        description=dict(type="str"),
+        othername=dict(type="str"),
+    ))
+
+required_if = [
+    ("state", "present", ("id", "name"), True),
+    ("state", "absent", ("id", "name"), True),
+]
+required_one_of = []
+mutually_exclusive = []
+required_together = []
+
+
+class NetworkDeviceGroup(object):
+    def __init__(self, params, ise):
+        self.ise = ise
+        self.new_object = dict(
+            id=params.get("id"),
+            name=params.get("name"),
+            description=params.get("description"),
+            othername=params.get("othername"),
+        )
+        #self.object_by_name = self.get_object_by_name(name)
+        #self.object_by_id = self.get_object_by_id(id)
+
+    def get_object_by_name(self, name):
+        try:
+            result = self.ise.exec(
+                family="network_device_group",
+                function="get_network_device_group_by_name",
+                params={"name": quote(name)}
+                ).response["NetworkDeviceGroup"]
+        except Exception as e:
+            result = None
+        return result
+
+    def get_object_by_id(self, id):
+        try:
+            result = self.ise.exec(
+                family="network_device_group",
+                function='get_network_device_group_by_id',
+                params={"id": quote(id)}
+            ).response["NetworkDeviceGroup"]
+        except Exception as e:
+            result = None
+        return result
+
+    def exists(self):
+        id_exists = False
+        name_exists = False
+        id = self.new_object.get("id")
+        name = self.new_object.get("name")
+        if id:
+            if self.get_object_by_id(id):
+                id_exists = True
+        if name:
+            if self.get_object_by_name(name):
+                name_exists = True
+        if id_exists and name_exists:
+            _id = self.get_object_by_name(name).get("id")
+            if id != id:
+                raise InconsistentParameters("The 'id' and 'name' params don't refer to the same object")
+        return id_exists or name_exists
+
+    def create(self):
+        result = self.ise.exec(
+            family="network_device_group", 
+            function="create_network_device_group",
+            params=self.new_object,
+        ).response
+        return result
+
+    def update(self):
+        id = self.new_object.get("id")
+        name = self.new_object.get("name")
+        result = None
+        if not id:
+            id = self.get_object_by_name(name).get("id")
+            self.new_object.update(dict(id=id))
+        result = self.ise.exec(
+            family="network_device_group",
+            function='update_network_device_group_by_id',
+            params=self.new_object
+        ).response            
+        return result
+
+    # TODO: Add logic to the delete operation, similar to the update operation
+    def delete(self):
+        id = self.new_object.get("id")
+        name = self.new_object.get("name")
+        result = None
+        if not id:
+            id = self.get_object_by_name(name).get("id")
+            self.new_object.update(dict(id=id))
+        result = self.ise.exec(
+            family="network_device_group",
+            function='delete_network_device_group_by_id',
+            params=self.new_object
+        ).response
+        return result
+
+
+class ActionModule(ActionBase):
+    def __init__(self, *args, **kwargs):
+        if not ANSIBLE_UTILS_IS_INSTALLED:
+            raise AnsibleActionFail("ansible.utils is not installed. Execute 'ansible-galaxy collection install ansible.utils'")
+        super(ActionModule, self).__init__(*args, **kwargs)
+        self._supports_async = False
+        self._result = None
+
+    # Checks the supplied parameters against the argument spec for this module
+    def _check_argspec(self):
+        aav = AnsibleArgSpecValidator(
+            data=self._task.args,
+            schema=dict(argument_spec=argument_spec),
+            schema_format="argspec",
+            schema_conditionals=dict(
+                required_if=required_if,
+                required_one_of=required_one_of,
+                mutually_exclusive=mutually_exclusive,
+                required_together=required_together,
+            ),
+            name=self._task.action,
+        )
+        valid, errors, self._task.args = aav.validate()
+        if not valid:
+            raise AnsibleActionFail(errors)
+
+    def run(self, tmp=None, task_vars=None):
+        self._task.diff = False
+        self._result = super(ActionModule, self).run(tmp, task_vars)
+        self._result["changed"] = False
+        self._check_argspec()
+
+        ise = ISESDK(self._task.args)
+        obj = NetworkDeviceGroup(self._task.args, ise)
+
+        state = self._task.args.get("state")
+
+        response = None
+
+        if state == "present":
+            if obj.exists():
+                response = obj.update()
+                ise.object_updated()
+            else:
+                response = obj.create()
+                ise.object_created()
+
+        elif state == "absent":
+            if obj.exists():
+                response = obj.delete()
+                ise.object_deleted()
+            else:
+                ise.object_already_absent()
+         
+        self._result.update(dict(ise_response=response))
+        self._result.update(ise.exit_json())
+        return self._result
