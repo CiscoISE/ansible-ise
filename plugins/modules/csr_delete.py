423ec4563d99175d51bc62b1b39937eac8f123f1
William Astorga <wastorga@altus.co.cr>
2021-05-28 17:57:28 -0600

    Adds CSR actions

---
+++ plugins/modules/csr_delete.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2021, Cisco Systems
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = r"""
+---
+module: csr_delete
+short_description: Resource module for Csr Delete
+description:
+- Manage operation delete of the resource Csr Delete.
+version_added: '1.0.0'
+author: Rafael Campos (@racampos)
+options:
+  hostName:
+    description: HostName path parameter. Name of the host of which CSR's should be
+      deleted.
+    type: str
+  id:
+    description: Id path parameter. The ID of the Certificate Signing Request to be
+      deleted.
+    type: str
+requirements:
+- ciscoisesdk
+seealso:
+# Reference by module name
+- module: cisco.ise.plugins.module_utils.definitions.csr_delete
+# Reference by Internet resource
+- name: Csr Delete reference
+  description: Complete reference of the Csr Delete object model.
+  link: https://ciscoisesdk.readthedocs.io/en/latest/api/api.html#v3-0-0-summary
+"""
+
+EXAMPLES = r"""
+- name: Delete by id
+  cisco.ise.csr_delete:
+    ise_hostname: "{{ise_hostname}}"
+    ise_username: "{{ise_username}}"
+    ise_password: "{{ise_password}}"
+    ise_verify: "{{ise_verify}}"
+    hostName: string
+    id: string
+
+"""
+
+RETURN = r"""
+ise_response:
+  description: A dictionary or list with the response returned by the Cisco ISE Python SDK
+  returned: always
+  type: dict
+  sample: >
+    {
+      "response": {
+        "message": "string"
+      },
+      "version": "string"
+    }
+"""
---
+++ plugins/modules/csr_generate.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2021, Cisco Systems
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = r"""
+---
+module: csr_generate
+short_description: Resource module for Csr Generate
+description:
+- Manage operation create of the resource Csr Generate.
+version_added: '1.0.0'
+author: Rafael Campos (@racampos)
+options:
+  allowWildCardCert:
+    description: AllowWildCardCert flag.
+    type: bool
+  certificatePolicies:
+    description: Csr Generate's certificatePolicies.
+    type: str
+  digestType:
+    description: Csr Generate's digestType.
+    type: str
+  hostnames:
+    description: Csr Generate's hostnames.
+    elements: str
+    type: list
+  keyLength:
+    description: Csr Generate's keyLength.
+    type: str
+  keyType:
+    description: Csr Generate's keyType.
+    type: str
+  portalGroupTag:
+    description: Csr Generate's portalGroupTag.
+    type: str
+  sanDNS:
+    description: Csr Generate's sanDNS.
+    elements: str
+    type: list
+  sanDir:
+    description: Csr Generate's sanDir.
+    elements: str
+    type: list
+  sanIP:
+    description: Csr Generate's sanIP.
+    elements: str
+    type: list
+  sanURI:
+    description: Csr Generate's sanURI.
+    elements: str
+    type: list
+  subjectCity:
+    description: Csr Generate's subjectCity.
+    type: str
+  subjectCommonName:
+    description: Csr Generate's subjectCommonName.
+    type: str
+  subjectCountry:
+    description: Csr Generate's subjectCountry.
+    type: str
+  subjectOrg:
+    description: Csr Generate's subjectOrg.
+    type: str
+  subjectOrgUnit:
+    description: Csr Generate's subjectOrgUnit.
+    type: str
+  subjectState:
+    description: Csr Generate's subjectState.
+    type: str
+  usedFor:
+    description: Csr Generate's usedFor.
+    type: str
+requirements:
+- ciscoisesdk
+seealso:
+# Reference by module name
+- module: cisco.ise.plugins.module_utils.definitions.csr_generate
+# Reference by Internet resource
+- name: Csr Generate reference
+  description: Complete reference of the Csr Generate object model.
+  link: https://ciscoisesdk.readthedocs.io/en/latest/api/api.html#v3-0-0-summary
+"""
+
+EXAMPLES = r"""
+- name: Create
+  cisco.ise.csr_generate:
+    ise_hostname: "{{ise_hostname}}"
+    ise_username: "{{ise_username}}"
+    ise_password: "{{ise_password}}"
+    ise_verify: "{{ise_verify}}"
+    allowWildCardCert: true
+    certificatePolicies: string
+    digestType: string
+    hostnames:
+    - string
+    keyLength: string
+    keyType: string
+    portalGroupTag: string
+    sanDNS:
+    - string
+    sanDir:
+    - string
+    sanIP:
+    - string
+    sanURI:
+    - string
+    subjectCity: string
+    subjectCommonName: string
+    subjectCountry: string
+    subjectOrg: string
+    subjectOrgUnit: string
+    subjectState: string
+    usedFor: string
+
+"""
+
+RETURN = r"""
+ise_response:
+  description: A dictionary or list with the response returned by the Cisco ISE Python SDK
+  returned: always
+  type: dict
+  sample: >
+    {
+      "response": [
+        {
+          "message": "string",
+          "id": "string",
+          "link": {
+            "rel": "string",
+            "href": "string",
+            "type": "string"
+          }
+        }
+      ],
+      "version": "string"
+    }
+"""
