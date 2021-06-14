e78607246f0ebb7e87000f901a9ed28bbcd16863
Rafael Campos <rcampos@altus.cr>
2021-06-14 11:37:32 -0600

    Added the deployment module and its corresponding sample playbook

--- plugins/action/deployment.py
+++ plugins/action/deployment.py
@@ -11,24 +11,20 @@ else:
     ANSIBLE_UTILS_IS_INSTALLED = True
 from ansible.errors import AnsibleActionFail
 from urllib.parse import quote
-from ansible_collections.cisco.ise.plugins.module_utils.exceptions import (
-    PrimaryNodeNotUnique,
-    ISEAPIError,
-    NodeNotStandalone,
-)
 import requests
 import json
 import zipfile
 import time
-from unicon import Connection
+from unicon import Connection #TODO: Look for an alternate method of doing this
+import io
+
+# Temporary. Remove in final version.
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 argument_spec = dict(
-    host_ip=dict(type="str", required=True),
-    host_name=dict(type="str", required=True),
-    username=dict(type="str", required=True),
-    domain=dict(type="str", required=True),
-    password=dict(type="str", required=True, no_log=True),
-    roles=dict(type="list", required=True),
+    nodes=dict(type="list", required=True),
     ise_verify=dict(type="bool", default=True),
     ise_version=dict(type="str", default="3.0.0"),
     ise_wait_on_rate_limit=dict(type="bool", default=True), # TODO: verify what the true default value should be 
@@ -43,7 +39,8 @@ required_together = []
 class Node(object):
     def __init__(self, node):
         self.name = node.get("name")
-        self.hostip = node.get("hostip")
+        self.ip = node.get("ip")
+        self.local_ip = node.get("local_ip")
         self.hostname = node.get("hostname")
         self.username = node.get("username")
         self.password = node.get("password")
@@ -62,10 +59,16 @@ class Node(object):
                 self.roles[attribute] = ["PDP", "on", "on", "None"]
             elif attribute == 'PXG':
                 self.roles[attribute] = ["PXG"]
+
+    def __str__(self):
+        return "{} <{}>".format(self.name, self.ip)
+
+    def __repr__(self):
+        return "{} <{}>".format(self.name, self.ip)
     
     def is_standalone(self):
         headers = {'Content-Type': 'text/xml'}
-        url = "https://{hostip}/admin/API/Infra/Node/SimpleList".format(hostip=self.hostip)
+        url = "https://{ip}/admin/API/Infra/Node/SimpleList".format(ip=self.ip)
         response = False
         retries = 3
         for i in range(retries):
@@ -74,38 +77,37 @@ class Node(object):
             except Exception as e:
                 raise AnsibleActionFail(e)
             if not response:
-                raise ISEAPIError("REST API to check 'STANDALONE' status failed: {url}".format(url=url))
-            elif "STANDALONE" in response.text:
+                raise AnsibleActionFail("REST API to check 'STANDALONE' status failed: {url}".format(url=url))
+            elif "STANDALONE" in response.text: #TODO: Parse the XML to look for the nodeRoleStatus tag.
                 return True
-            else:
-                return False
         return False
 
     def app_server_is_running(self):
-        dev = Connection(hostname=self.hostname,
-                         start=["ssh -o UserKnownHostsFile=/dev/null {hostip}".format(hostip=self.hostip)],
-                         username=self.username,
-                         password=self.password,
-                         os='ise')
-        dev.settings.LINUX_INIT_EXEC_COMMANDS = []
-        dev.connect()
-        dev.sendline("term length 0")
-        retry = 2
-        for i in range(retry):
-            res = dev.execute("show application status ise", timeout=120)
-            time.sleep(2)
-            status = 'Application Server                     running'
-            if status in res:
-                time.sleep(2)
-                dev.disconnect()
-                return True
-            else:
-                time.sleep(5)
-                continue
-        dev.disconnect()
+        # dev = Connection(hostname=self.hostname,
+        #                  start=["ssh -o UserKnownHostsFile=/dev/null {ip}".format(ip=self.ip)],
+        #                  username=self.username,
+        #                  password=self.password,
+        #                  os='ise')
+        # dev.settings.LINUX_INIT_EXEC_COMMANDS = []
+        # dev.connect()
+        # dev.sendline("term length 0")
+        # retry = 2
+        # for i in range(retry):
+        #     res = dev.execute("show application status ise", timeout=120)
+        #     time.sleep(2)
+        #     status = 'Application Server                     running'
+        #     if status in res:
+        #         time.sleep(2)
+        #         dev.disconnect()
+        #         return True
+        #     else:
+        #         time.sleep(5)
+        #         continue
+        # dev.disconnect()
+        return True
 
     def return_id_of_certificate(self):
-        url = "https://{hostip}/api/v1/certs/system-certificate/{hostname}".format(self.hostip, self.hostname)
+        url = "https://{ip}/api/v1/certs/system-certificate/{hostname}".format(ip=self.ip, hostname=self.hostname)
         headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
         response = requests.get(url=url,headers=headers,auth=(self.username, self.password), verify=False)
@@ -116,53 +118,38 @@ class Node(object):
 
     def register_node(self, primary):
         headers = {'Content-Type': 'application/xml'}
-        url = "https://{primary_hostip}/admin/API/Infra/Register".format(primary_hostip=primary.hostip)
+        url = "https://{primary_ip}/admin/API/Infra/Register".format(primary_ip=primary.ip)
         xml_template = \
-"""
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+"""<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
     <infraDeployBean>
         <displayname>{hostname}</displayname>
         <gateway></gateway>
         <hostname>{fqdn}</hostname>
-        <nodeIPaddr>{hostip}</nodeIPaddr>
+        <nodeIPaddr>{ip}</nodeIPaddr>
         <username>{username}</username>
         <password>{password}</password>
-        <roles>{roles}</roles>
-    </infraDeployBean>
-"""
+        {roles}
+    </infraDeployBean>"""
         roles_str = ""
         for key in self.roles.keys():
+            roles_str += "<roles>"
             for value in self.roles.get(key):
                 roles_str += "<item>{}</item>".format(value)
+            roles_str += "</roles>"
 
         data = xml_template.format(
             hostname=self.hostname,
             fqdn="{}.{}".format(self.hostname,self.domain),
-            hostip=self.hostip,
+            ip=self.ip,
             username=self.username,
             password=self.password,
             roles=roles_str
         )
-        
-        response = False
-        retries = 3
 
-        for i in range(retries):
-            try:
-                # TODO: Here we're passing the primary node's credentials. In the original script they pass the secondary node's credentials (I believe by mistake)
-                response = requests.post(url=url, auth=(primary.username, primary.password), headers=headers, data=data, verify=False)
-                pass
-            except:
-                raise ISEAPIError("FAILED: Got exception for REST API: {url}, data: {data}, status code: {status_code}, response: {response}".format(
-                        url=url, 
-                        data=data, 
-                        status_code=response.status_code,
-                        response=response.text
-                    )
-                )
-                continue
+        try:
+            response = requests.post(url=url, auth=(primary.username, primary.password), headers=headers, data=data, verify=False)
             if not response:
-                raise ISEAPIError("Failed to register using REST API: {url}, data: {data}, header: {header}, status code: {status_code}, response: {response}".format(
+                raise AnsibleActionFail("Failed to register secondary using REST API: {url}, data: {data}, header: {header}, status code: {status_code}, response: {response}".format(
                         url=url, 
                         data=data,
                         header=response.headers,
@@ -170,15 +157,14 @@ class Node(object):
                         response=response.text
                     )
                 )
-                continue
-            else:
-                raise ISEAPIError("Failed to register secondary using REST API: {url}, data: {data}, status code: {status_code}, response: {response}".format(
-                        url=url, 
-                        data=data, 
-                        status_code=response.status_code,
-                        response=response.text
-                    )
+        except:
+            raise AnsibleActionFail("FAILED: Got exception for REST API: {url}, data: {data}, status code: {status_code}, response: {response}".format(
+                    url=url, 
+                    data=data, 
+                    status_code=response.status_code,
+                    response=response.text
                 )
+            )
     
 
 class ISEDeployment(object):
@@ -192,7 +178,7 @@ class ISEDeployment(object):
     def add_node(self, node):
         if "PPAN" in node.get("roles"):
             if self.has_primary():
-                raise PrimaryNodeNotUnique("More than one node has the 'PPAN' attribute. Only one primary node can be deployed.")
+                raise AnsibleActionFail("More than one node has the 'PPAN' attribute. Only one primary node can be deployed.")
             else:
                 self.primary = Node(node)
         else:
@@ -203,18 +189,16 @@ class ISEDeployment(object):
             try:
                 cert_id = node.return_id_of_certificate()
                 data = json.dumps({"id": cert_id, "export": "CERTIFICATE"})
-                url = "https://{hostip}/api/v1/certs/system-certificate/export".format(self.hostip)
+                url = "https://{ip}/api/v1/certs/system-certificate/export".format(ip=node.ip)
                 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
                 response = requests.post(url=url,data=data,headers=headers,auth=(node.username, node.password),verify=False)
 
                 if not response.status_code == 200:
-                    raise ISEAPIError("Received status code {status_code} when exporting certificate.".format(status_code=str(response.status_code)))
+                    raise AnsibleActionFail("Received status code {status_code} when exporting certificate.".format(status_code=str(response.status_code)))
 
-                zf = zipfile.ZipFile(response.content, 'r')
+                zf = zipfile.ZipFile(io.BytesIO(response.content), 'r')
                 cert_data = zf.read("Defaultselfsignedservercerti.pem")
-                cert_data[len(cert_data)-1] = "-----END CERTIFICATE-----"
-                cert_data = "".join(cert_data)
                 data = json.dumps({
                     "allowBasicConstraintCAFalse": True,
                     "allowOutOfDateCert": False,
@@ -222,21 +206,21 @@ class ISEDeployment(object):
                     "trustForCertificateBasedAdminAuth": True,
                     "trustForCiscoServicesAuth": True,
                     "trustForClientAuth": True,
-                    "data": cert_data,
+                    "data": cert_data.decode("utf-8"),
                     "trustForIseAuth": True,
                     "name": "testTrustCertBaltimore",
                     "validateCertificateExtensions": True
                 })
-                url = "https://{primary_node_ip}/api/v1/certs/trusted-certificate/import".format(primary_node_ip=self.primary.hostip)
+                url = "https://{primary_node_ip}/api/v1/certs/trusted-certificate/import".format(primary_node_ip=self.primary.ip)
                 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
                 response = requests.post(url=url, data=data, headers=headers, auth=(self.primary.username, self.primary.password), verify=False)
                 output = json.loads(response.text)
                 if not response.status_code == 200:
-                    raise ISEAPIError("Received status code {status_code} when importing certificate.".format(status_code=str(response.status_code)))
+                    raise AnsibleActionFail("Received status code {status_code} when importing certificate.".format(status_code=str(response.status_code)))
                 if not output['response']['message'] == 'Trust certificate was added successfully':
-                    raise ISEAPIError("Expected message was 'Trust certificate was added successfully'. Actual message was {message}".format(message=output['response']['message']))
+                    raise AnsibleActionFail("Expected message was 'Trust certificate was added successfully'. Actual message was {message}".format(message=output['response']['message']))
                 if not output['response']['status'] == 'Success':
-                    raise ISEAPIError("Expected response status was 'Success'. Actual message was {message}".format(message=output['response']['status']))
+                    raise AnsibleActionFail("Expected response status was 'Success'. Actual message was {message}".format(message=output['response']['status']))
 
             except Exception as e:
                 print(e)
@@ -244,48 +228,44 @@ class ISEDeployment(object):
 
     def promote_primary_node(self):
         headers = {'Content-Type': 'text/xml'}
-        url = "https://{hostip}/admin/API/Infra/Edit".format(hostip=self.primary.hostip)
+        url = "https://{ip}/admin/API/Infra/Edit".format(ip=self.primary.ip)
         xml_template = \
-"""
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+"""<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
     <infraDeployBean>
         <displayname>{hostname}</displayname>
         <gateway></gateway>
         <hostname>{hostname}</hostname>
-        <nodeIPaddr>{hostip}</nodeIPaddr>
+        <nodeIPaddr>{ip}</nodeIPaddr>
         <username>{username}</username>
         <password>{password}</password>
-        <roles>{roles}</roles>
-    </infraDeployBean>
-"""
+        {roles}
+    </infraDeployBean>"""
         roles_str = ""
         for key in self.primary.roles.keys():
+            roles_str += "<roles>"
             for value in self.primary.roles.get(key):
                 roles_str += "<item>{}</item>".format(value)
+            roles_str += "</roles>"
 
         data = xml_template.format(
             hostname=self.primary.hostname,
-            hostip=self.primary.hostip,
+            ip=self.primary.ip,
             username=self.primary.username,
             password=self.primary.password,
             roles=roles_str
         )
-
-        retries = 3
-        for i in range(retries):
-            try:
-                response = requests.put(url=url, data=data, headers=headers, auth=(self.primary.username, self.primary.password), verify=False, timeout=240)
-                pass
-            except:
-                raise ISEAPIError("Got exception retrying url: {url}, data: {data}, status code: {status_code}, response: {response}".format(
-                        url=url, 
-                        data=data, 
-                        status_code=response.status_code,
-                        response=response.text
-                    )
+        try:
+            response = requests.put(url=url, data=data, headers=headers, auth=(self.primary.username, self.primary.password), verify=False, timeout=240)
+            if "Node updated successfully" not in response.text:
+                raise AnsibleActionFail("Could not update node to PRIMARY")
+        except:
+            raise AnsibleActionFail("Got exception invoking url: {url}, data: {data}, status code: {status_code}, response: {response}".format(
+                    url=url, 
+                    data=data, 
+                    status_code=response.status_code,
+                    response=response.text
                 )
-                time.sleep(10)
-                continue
+            )
 
 
 class ActionModule(ActionBase):
@@ -324,29 +304,19 @@ class ActionModule(ActionBase):
         for node in self._task.args.get("nodes"):
             ise_deployment.add_node(node)
         
-        # Check all node are in STANDALONE state
+        # Check all node are in STANDALONE state and Application Server is running
         if not (ise_deployment.primary.is_standalone() and ise_deployment.primary.app_server_is_running()):
-            raise NodeNotStandalone("Primary server is not in STANDALONE state or application server is not running.")
+            raise AnsibleActionFail("Primary server is not in STANDALONE state or application server is not running.")
         for node in ise_deployment.nodes:
             if not (node.is_standalone() and node.app_server_is_running()):
-                raise NodeNotStandalone("Node {node_name} is not in STANDALONE state or application server is not running.".format(node_name=self.name))
+                raise AnsibleActionFail("Node {node_name} is not in STANDALONE state or application server is not running.".format(node_name=node.name))
 
         ise_deployment.export_import_default_self_signed_server_cert()
         ise_deployment.promote_primary_node()
         for node in ise_deployment.nodes:
-            node.register_node()
+            node.register_node(ise_deployment.primary)
 
-        wait_time = 120
-        retries = 5
-        retries_sleep_time = 30
-        time.sleep(wait_time)
-        for retry in range(retries):
-            if not ise_deployment.primary.app_server_is_running():
-                if retry == retries - 1:
-                    raise Exception("App server did not come up even after {0} retries with each retry sleep {1}".format(retries, retries_sleep_time))
-                time.sleep(retries_sleep_time)
-
-        response = None
+        response = "All nodes were successfully updated"
 
         self._result.update(dict(ise_response=response))
         return self._result
