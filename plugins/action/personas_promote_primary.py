d7a87ca7979e90e22836542e56de0f395d978e8c
Rafael Campos <rcampos@altus.cr>
2021-07-12 13:58:43 -0600

    Split the 'deployment' module into four simpler modules and a 'utilities' file

---
+++ plugins/action/personas_promote_primary.py
@@ -0,0 +1,88 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from ansible.plugins.action import ActionBase
+try:
+    from ansible_collections.ansible.utils.plugins.module_utils.common.argspec_validate import (
+        AnsibleArgSpecValidator,
+    )
+except ImportError:
+    ANSIBLE_UTILS_IS_INSTALLED = False
+else:
+    ANSIBLE_UTILS_IS_INSTALLED = True
+from ansible.errors import AnsibleActionFail
+from urllib.parse import quote
+import time
+from ansible_collections.cisco.ise.plugins.module_utils.personas_utils import (
+    Node,
+    ISEDeployment,
+) 
+
+argument_spec = dict(
+    ip=dict(type="str", required=True),
+    hostname=dict(type="str", required=True),
+    username=dict(type="str", required=True),
+    password=dict(type="str", required=True),
+    roles=dict(type="list", required=True),
+    ise_verify=dict(type="bool", default=True),
+    ise_version=dict(type="str", default="3.0.0"),
+    ise_wait_on_rate_limit=dict(type="bool", default=True), # TODO: verify what the true default value should be 
+)
+
+required_if = []
+required_one_of = []
+mutually_exclusive = []
+required_together = []
+
+
+class ActionModule(ActionBase):
+    def __init__(self, *args, **kwargs):
+        if not ANSIBLE_UTILS_IS_INSTALLED:
+            raise AnsibleActionFail("ansible.utils is not installed. Execute 'ansible-galaxy collection install ansible.utils'")
+        super(ActionModule, self).__init__(*args, **kwargs)
+        self._supports_async = False
+        self._result = None
+
+    # Checks the supplied parameters against the argument spec for this module
+    def _check_argspec(self):
+        aav = AnsibleArgSpecValidator(
+            data=self._task.args,
+            schema=dict(argument_spec=argument_spec),
+            schema_format="argspec",
+            schema_conditionals=dict(
+                required_if=required_if,
+                required_one_of=required_one_of,
+                mutually_exclusive=mutually_exclusive,
+                required_together=required_together,
+            ),
+            name=self._task.action,
+        )
+        valid, errors, self._task.args = aav.validate()
+        if not valid:
+            raise AnsibleActionFail(errors)
+
+    def run(self, tmp=None, task_vars=None):
+        self._task.diff = False
+        self._result = super(ActionModule, self).run(tmp, task_vars)
+        self._result["changed"] = False
+        self._check_argspec()
+
+        primary_node = dict(ip=self._task.args.get("ip"),
+                            hostname=self._task.args.get("hostname"),
+                            username=self._task.args.get("username"),
+                            password=self._task.args.get("password"),
+                            roles=self._task.args.get("roles"),
+                        )
+        
+        if "PPAN" not in primary_node.get("roles"):
+            raise AnsibleActionFail("Primary node must have at least the 'PPAN' role")
+        
+        ise_deployment = ISEDeployment()
+        ise_deployment.add_primary(primary_node)
+
+        ise_deployment.promote_primary_node()
+
+
+        response = "Primary node was successfully updated"
+
+        self._result.update(dict(ise_response=response))
+        return self._result
