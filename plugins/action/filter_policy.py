1b6f2ad1726582a91922f3ba3af66fea941d704e
William Astorga <wastorga@altus.co.cr>
2021-07-27 15:28:18 -0600

    Remove duplicate except handling

--- plugins/action/filter_policy.py
+++ plugins/action/filter_policy.py
@@ -209,16 +209,6 @@ class FilterPolicy(object):
                     " The error was: {error}"
                 ).format(error=e)
             )
-        except (TypeError, AttributeError) as e:
-            self.ise.fail_json(
-                msg=(
-                    "An error occured when executing operation."
-                    " Check the configuration of your API Settings and API Gateway settings on your ISE server."
-                    " This collection assumes that the API Gateway, the ERS APIs and OpenAPIs are enabled."
-                    " You may want to enable the (ise_debug: True) argument."
-                    " The error was: {error}"
-                ).format(error=e)
-            )
         except Exception:
             result = None
         return result
