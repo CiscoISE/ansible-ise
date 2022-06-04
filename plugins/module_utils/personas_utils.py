2214868f49d85a08619ab46e7147ab2cf2e23b97
Rafael Campos <rcampos@altus.cr>
2022-06-03 23:55:58 -0600

    Creates the 'personas_update_roles_services', which uses the public deployment API to update the roles and services of a node

--- plugins/module_utils/personas_utils.py
+++ plugins/module_utils/personas_utils.py
@@ -22,7 +22,6 @@ class Node(object):
     def __init__(self, node):
         self.name = node.get("name")
         self.ip = node.get("ip")
-        self.local_ip = node.get("local_ip")
         self.hostname = node.get("hostname")
         self.username = node.get("username")
         self.password = node.get("password")
@@ -101,6 +100,21 @@ class Node(object):
             raise AnsibleActionFail(e)
         if not response:
             raise AnsibleActionFail("Failed to receive a valid response from the API. The actual response was: {response}".format(response=response.text))
+
+    def update_roles_services(self):
+        headers = {'Content-Type': 'application/json'}
+        url = "https://{ip}/api/v1/deployment/node/{hostname}".format(ip=self.ip, hostname=self.hostname)
+        data = json.dumps({
+            "roles": self.roles,
+            "services": self.services
+        })
+        try:
+            response = requests.put(url=url, timeout=300, auth=(self.username, self.password), headers=headers, data=data, verify=False)
+        except Exception as e:
+            raise AnsibleActionFail(e)
+        if not response:
+            raise AnsibleActionFail("Failed to receive a valid response from the API. The actual response was: {response}".format(response=response.text))
+
 
 
 class ISEDeployment(object):
