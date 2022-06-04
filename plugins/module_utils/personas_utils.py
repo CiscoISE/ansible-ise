ab46884742f1867804cf820d85ce5fff38d11ea1
Rafael Campos <rcampos@altus.cr>
2022-06-04 14:07:27 -0600

    Refactors the 'personas_promote_primary' module

--- plugins/module_utils/personas_utils.py
+++ plugins/module_utils/personas_utils.py
@@ -155,23 +155,11 @@ class Node(object):
                     return_message == "Certificates are having same subject, same serial number and they are binary equal. Hence skipping the replace"):
                 raise AnsibleActionFail("Unexpected response from API. Received response was {message}".format(message=return_message))
 
-
-class ISEDeployment(object):
-    def __init__(self):
-        self.primary = None
-        self.nodes = []
-
-    def add_primary(self, node):
-        self.primary = Node(node)
-
-    def add_node(self, node):
-        self.nodes.append(Node(node))
-
-    def promote_primary_node(self):
+    def promote_to_primary(self):
         headers = {'Content-Type': 'application/json'}
-        url = "https://{ip}/api/v1/deployment/primary".format(ip=self.primary.ip)
+        url = "https://{ip}/api/v1/deployment/primary".format(ip=self.ip)
         try:
-            response = requests.post(url=url, headers=headers, auth=(self.primary.username, self.primary.password), verify=False, timeout=60)
+            response = requests.post(url=url, headers=headers, auth=(self.username, self.password), verify=False, timeout=60)
             if response.status_code == 200:
                 return True
             else:
