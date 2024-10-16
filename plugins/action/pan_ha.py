51ccf3e9728153d91e1f5c1627f60dd21900f607
Francisco Muñoz <fmunoz@cloverhound.com>
2024-10-16 11:06:35 -0600

    Collection not compatible with ansible.utils 5.x.y #144 getting deployment info for entire deployment does not work #142 cisco.ise.pan_ha object has no attribute 'enable_pan_ha' #141

--- plugins/action/pan_ha.py
+++ plugins/action/pan_ha.py
@@ -90,7 +90,8 @@ class PanHa(object):
         if name_exists:
             _id = prev_obj.get("id")
             if id_exists and name_exists and o_id != _id:
-                raise InconsistentParameters("The 'id' and 'name' params don't refer to the same object")
+                raise InconsistentParameters(
+                    "The 'id' and 'name' params don't refer to the same object")
         it_exists = prev_obj is not None and isinstance(prev_obj, dict)
         return (it_exists, prev_obj)
 
@@ -133,7 +134,8 @@ class PanHa(object):
 class ActionModule(ActionBase):
     def __init__(self, *args, **kwargs):
         if not ANSIBLE_UTILS_IS_INSTALLED:
-            raise AnsibleActionFail("ansible.utils is not installed. Execute 'ansible-galaxy collection install ansible.utils'")
+            raise AnsibleActionFail(
+                "ansible.utils is not installed. Execute 'ansible-galaxy collection install ansible.utils'")
         super(ActionModule, self).__init__(*args, **kwargs)
         self._supports_async = False
         self._supports_check_mode = False
@@ -173,8 +175,21 @@ class ActionModule(ActionBase):
             (obj_exists, prev_obj) = obj.exists()
             if obj_exists:
                 if obj.requires_update(prev_obj):
-                    response = prev_obj
-                    ise.object_present_and_different()
+                    ise_update_response = obj.update()
+                    self._result.update(
+                        dict(ise_update_response=ise_update_response))
+                    (obj_exists, updated_obj) = obj.exists()
+                    response = updated_obj
+                    has_changed = None
+                    has_changed = ise_update_response.get(
+                        "UpdatedFieldsList").get("updatedField")
+                    if (len(has_changed) == 0 or
+                       has_changed[0].get("newValue") == "" and
+                       has_changed[0].get("newValue") == has_changed[0].get("oldValue")):
+                        self._result.pop("ise_update_response", None)
+                        ise.object_already_present()
+                    else:
+                        ise.object_updated()
                 else:
                     response = prev_obj
                     ise.object_already_present()
